6f7f8796a79c2ccd5fa5c0c84aa6f1110f1a5177
Thierry Laporte <laportethierry@yahoo.fr>
2020-11-11 18:42:49 +0100

    Restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant = Restaurant.create([
+  {
+    name: "La Cave à Michel",
+    address: "36, rue Saint-Marthe - 75010 Paris",
+    category: "italian"
+  },
+  {
+    name: "La Scène",
+    address: "32, avenue Matignon - 75008 Paris",
+    category: "french"
+  },
+  {
+    name: "Pierre Sang Boyer",
+    address: "55, rue oberkampf - 75011 Paris",
+    category: "french"
+  },
+  {
+    name: "Pierre Sang on Gambey",
+    address: "6, rue Gambey - 75011 Paris",
+    category: "chinese"
+  },
+  {
+    name: "B.B. le Restaurant",
+    address: "21, rue Blanche - 75009 Paris",
+    category: "belgian"
+  }
+])
